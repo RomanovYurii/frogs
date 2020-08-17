5471095c68a7aa7df2366ebfafbb5bab637e2172
Romanov Yurii <ranoveria@gmail.com>
2020-08-17 14:40:17 +0200

    Created unit component(s): Description Table

---
+++ src/components/2_units/Table/Table.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Table.styles.scss';
+
+const Table = () => {
+	return <div></div>;
+};
+
+export default Table;
--- src/components/4_ecosystems/App/App.js
+++ src/components/4_ecosystems/App/App.js
@@ -1,33 +1,12 @@
 import React from 'react';
 import logo from '../../../logo.svg';
 import './App.styles.scss';
+import Description from '../../0_cells/Description';
 
 function App() {
 	return (
 		<>
-			<div className="description">
-				Below is a lake with dimensions 10x6 fields. Frogs are marked as green rectangles. Frog with a small
-				blue rectangle is a male; with a purple rectangle female.<br/><br/>
-
-				<strong>Acceptance criteria:</strong><br/>
-				<ul>
-					<li>Each frog can jump on an empty field (select the frog, the empty field and click the jump
-						button)
-					</li>
-					<li>A male frog can jump 3 fields (also diagonal)</li>
-					<li>A female frog can jump 2 fields (also diagonal)</li>
-					<li>Each frog should have two characteristics (array of two elements: tall, short, fat, slim)</li>
-					<li>Two frogs different genders, adjacent can reproduce (select one frog a male, one female and
-						click the reproduce button)
-					</li>
-					<li>The new frog should be placed in the first available space adjacent to the mother</li>
-					<li>The new frog should have one characteristic from mother and one from father</li>
-					<li>You can use PureJS, AngularJS, ReactJS to solve this one. It's up to you</li>
-					<li>Please don't spend more than 5 hours on this task.</li>
-				</ul>
-
-				<strong>Good Luck :)</strong>
-			</div>
+			<Description />
 
 			<table id="lake">
 				<thead>
@@ -36,315 +15,315 @@ function App() {
 					</tr>
 				</thead>
 				<tbody>
-				<tr>
-					<td>
-						<label className="frog male">
-							<input type="checkbox"/>
-						</label>
-					</td>
-					<td>
-						<label className="frog female">
-							<input type="checkbox"/>
-						</label>
-					</td>
-					<td>
-						<label>
-							<input type="checkbox"/>
-						</label>
-					</td>
-					<td>
-						<label>
-							<input type="checkbox"/>
-						</label>
-					</td>
-					<td>
-						<label>
-							<input type="checkbox"/>
-						</label>
-					</td>
-					<td>
-						<label>
-							<input type="checkbox"/>
-						</label>
-					</td>
-					<td>
-						<label>
-							<input type="checkbox"/>
-						</label>
-					</td>
-					<td>
-						<label>
-							<input type="checkbox"/>
-						</label>
-					</td>
-					<td>
-						<label>
-							<input type="checkbox"/>
-						</label>
-					</td>
-					<td>
-						<label>
-							<input type="checkbox"/>
-						</label>
-					</td>
-					</tr>
 					<tr>
 						<td>
-							<label>
-								<input type="checkbox"/>
+							<label className="frog male">
+								<input type="checkbox" />
+							</label>
+						</td>
+						<td>
+							<label className="frog female">
+								<input type="checkbox" />
 							</label>
 						</td>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 						<td>
 							<label>
-								<input type="checkbox"/>
-							</label>
-						</td>
-						<td>
-							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 					</tr>
 					<tr>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 						<td>
 							<label>
-								<input type="checkbox"/>
-							</label>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<label>
-								<input type="checkbox"/>
-							</label>
-						</td>
-						<td>
-							<label>
-								<input type="checkbox"/>
-							</label>
-						</td>
-						<td>
-							<label>
-								<input type="checkbox"/>
-							</label>
-						</td>
-						<td>
-							<label>
-								<input type="checkbox"/>
-							</label>
-						</td>
-						<td>
-							<label>
-								<input type="checkbox"/>
-							</label>
-						</td>
-						<td>
-							<label>
-								<input type="checkbox"/>
-							</label>
-						</td>
-						<td>
-							<label>
-								<input type="checkbox"/>
-							</label>
-						</td>
-						<td>
-							<label>
-								<input type="checkbox"/>
-							</label>
-						</td>
-						<td>
-							<label>
-								<input type="checkbox"/>
-							</label>
-						</td>
-						<td>
-							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 					</tr>
 					<tr>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 					</tr>
 					<tr>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
 							</label>
 						</td>
 						<td>
 							<label>
-								<input type="checkbox"/>
+								<input type="checkbox" />
+							</label>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<label>
+								<input type="checkbox" />
+							</label>
+						</td>
+						<td>
+							<label>
+								<input type="checkbox" />
+							</label>
+						</td>
+						<td>
+							<label>
+								<input type="checkbox" />
+							</label>
+						</td>
+						<td>
+							<label>
+								<input type="checkbox" />
+							</label>
+						</td>
+						<td>
+							<label>
+								<input type="checkbox" />
+							</label>
+						</td>
+						<td>
+							<label>
+								<input type="checkbox" />
+							</label>
+						</td>
+						<td>
+							<label>
+								<input type="checkbox" />
+							</label>
+						</td>
+						<td>
+							<label>
+								<input type="checkbox" />
+							</label>
+						</td>
+						<td>
+							<label>
+								<input type="checkbox" />
+							</label>
+						</td>
+						<td>
+							<label>
+								<input type="checkbox" />
+							</label>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<label>
+								<input type="checkbox" />
+							</label>
+						</td>
+						<td>
+							<label>
+								<input type="checkbox" />
+							</label>
+						</td>
+						<td>
+							<label>
+								<input type="checkbox" />
+							</label>
+						</td>
+						<td>
+							<label>
+								<input type="checkbox" />
+							</label>
+						</td>
+						<td>
+							<label>
+								<input type="checkbox" />
+							</label>
+						</td>
+						<td>
+							<label>
+								<input type="checkbox" />
+							</label>
+						</td>
+						<td>
+							<label>
+								<input type="checkbox" />
+							</label>
+						</td>
+						<td>
+							<label>
+								<input type="checkbox" />
+							</label>
+						</td>
+						<td>
+							<label>
+								<input type="checkbox" />
+							</label>
+						</td>
+						<td>
+							<label>
+								<input type="checkbox" />
 							</label>
 						</td>
 					</tr>
@@ -365,10 +344,14 @@ function App() {
 				</ul>
 
 				<h3>Actions</h3>
-				<button type="button" id="jump">Jump</button>
-				<button type="button" id="reproduce">Reproduce</button>
+				<button type="button" id="jump">
+					Jump
+				</button>
+				<button type="button" id="reproduce">
+					Reproduce
+				</button>
 			</div>
-			</>
+		</>
 	);
 }
 
