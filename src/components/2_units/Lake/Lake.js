64ad3135975b911d43845481791095a6181577ae
Romanov Yurii <ranoveria@gmail.com>
2020-08-17 14:43:44 +0200

    Created unit component(s): Lake

---
+++ src/components/2_units/Lake/Lake.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Lake.styles.scss';
+
+const Lake = () => {
+	return <div></div>;
+};
+
+export default Lake;
--- src/components/2_units/Legend/Legend.js
+++ src/components/2_units/Legend/Legend.js
@@ -2,7 +2,29 @@ import React from 'react';
 import './Legend.styles.scss';
 
 const Legend = () => {
-	return <div></div>;
+	return (
+		<div className="legend">
+			<h3>Legend</h3>
+			<ul>
+				<li>
+					<span className="frog male"></span>
+					<strong>Frog male</strong>
+				</li>
+				<li>
+					<span className="frog female"></span>
+					<strong>Frog female</strong>
+				</li>
+			</ul>
+
+			<h3>Actions</h3>
+			<button type="button" id="jump">
+				Jump
+			</button>
+			<button type="button" id="reproduce">
+				Reproduce
+			</button>
+		</div>
+	);
 };
 
 export default Legend;
